f431f84999e456b51c503fd6f3718eebfb05a98a
Zack Werner <allthelight044@gmail.com>
2021-02-07 11:01:33 -0600

    added a retrieveOp to grab the values from the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,16 +2,25 @@ console.log('hello from client.js');
 
 $(document).ready(onReady);
 
+let mathOperator = ''; // place to store a global operator
+
 function onReady() {
   console.log('jQuery is working');
 
-  $(document).on('click', '#numOne', retrieveMaths);
+  $(document).on('click', '.operatorInput', retrieveOperator);
 
-  $(document).on('submit', '#equals', submitAction);
+  $('#equals').on('click', retrieveMaths);
 
-  $(document).on('reset', '#clearBtn', clearForm);
+  // $(document).on('reset', '#clearBtn', clearForm);
 }
 // this info is being pulled from the DOM.
+
+function retrieveOperator(event) {
+  event.preventDefault();
+  mathOperator = this.value;
+  console.log('Clicked operator: ', this.value);
+}
+
 function retrieveMaths(event) {
   event.preventDefault();
   console.log('inside the retrieveMaths func');
@@ -21,49 +30,42 @@ function retrieveMaths(event) {
     // I will then be able to send the info to server side.
     {
       enterNum1: Number($('#numOne').val()),
-      additionValue: $('#addInput').val(),
-      minusValue: $('#minusInput').val(),
-      multiValue: $('#multiInput').val(),
-      divideValue: $('#divideInput').val(),
       enterNum2: Number($('#numTwo').val()),
+      theOperator: mathOperator, // this is coming from the top of the file.
     },
   ];
+
+  // submitValue: $('#equals').val(),
+  // resetValue: $('#clearBtn').val(),
+
   console.log('Array info', retrieveInfoArray);
-
-  function submitAction(event) {
-    event.preventDefault();
-    console.log('inside submitAction func');
-
-    // submitValue: $('#equals').val(),
-    // resetValue: $('#clearBtn').val(),
-  }
-  //
-  //
-  function clearForm(event) {
-    event.preventDefault();
-    // this is going to clear the inputs
-    $('#numOne').val('');
-    $('#addInput').val('');
-    $('#minusInput').val('');
-    $('#multiInput').val('');
-    $('#divideInput').val('');
-  }
-  //
-  //
-  // POST quote data to server
-  $.ajax({
-    url: '/calculator',
-    method: 'POST',
-    // this becomes .req.body - data:
-    data: {
-      items_to_equate: retrieveInfoArray,
-    },
-  })
-    .then(function () {
-      console.log('Huzzah!');
-      // not sure what to do with this yet just have it ...
-    })
-    .catch(function (error) {
-      console.log('Wah Wah', error);
-    });
 }
+//
+//
+function clearForm(event) {
+  event.preventDefault();
+  // this is going to clear the inputs
+  $('#numOne').val('');
+  $('#addInput').val('');
+  $('#minusInput').val('');
+  $('#multiInput').val('');
+  $('#divideInput').val('');
+}
+//
+//
+// POST quote data to server
+// $.ajax({
+//   url: '/calculator',
+//   method: 'POST',
+//   // this becomes .req.body - data:
+//   data: {
+//     items_to_equate: retrieveInfoArray,
+//   },
+// })
+//   .then(function () {
+//     console.log('Huzzah!');
+//     // not sure what to do with this yet just have it ...
+//   })
+//   .catch(function (error) {
+//     console.log('Wah Wah', error);
+//   });
